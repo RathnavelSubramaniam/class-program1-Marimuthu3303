22c96e230430c1eb8dec84e1d73d8bd1811636b4
Marimuthu3303 <142867529+Marimuthu3303@users.noreply.github.com>
2023-08-23 10:56:25 +0530

    Create Season.java
    Com

---
+++ Season.java
@@ -0,0 +1,26 @@
+class ifelse{
+  public static void main (String args[]){
+    int month=4;
+    string season;
+    if(month==12||month==1||month==2)
+    {
+      season="winter";
+    }
+    elseif(month==3||month==4||month==5)
+      {
+      season="spring";
+    }
+    esleif(month==6||month==7||month==8)
+      {
+      season="summer";
+    }
+    elseif(month==9||month==10||month==11)
+      {
+      season="autumn";
+    }
+    esle 
+      {season="bogus month";
+  }
+  System.out.println("April is in the"+season);
+      }
+}
